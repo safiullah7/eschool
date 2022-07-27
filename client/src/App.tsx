e38b508729316545edfbc28ff0b3b37320c1f54d
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-07-27 17:20:53 +0500

    Adding dark-light mode functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import DarkMode from './components/darkMode/DarkMode';
 
 function App() {
   return (
     <div className="App">
       Hello World
+      <DarkMode />
     </div>
   );
 }
---
+++ client/src/components/darkMode/DarkMode.tsx
@@ -0,0 +1,42 @@
+
+const DarkMode = () => {
+  let clickedClass = "clicked";
+  const body = document.body;
+  const lightTheme = "light";
+  const darkTheme = "dark";
+  let theme:any
+
+  if (localStorage) {
+    theme = localStorage.getItem("theme");
+  }
+
+  if (theme === lightTheme || theme === darkTheme) {
+    body.classList.add(theme);
+  } else {
+    body.classList.add(lightTheme);
+  }
+
+  const switchTheme = (e:any) => {
+    if (theme === darkTheme) {
+      body.classList.replace(darkTheme, lightTheme);
+      e.target.classList.remove(clickedClass);
+      localStorage.setItem("theme", "light");
+      theme = lightTheme;
+    } else {
+      body.classList.replace(lightTheme, darkTheme);
+      e.target.classList.add(clickedClass);
+      localStorage.setItem("theme", "dark");
+      theme = darkTheme;
+    }
+  };
+
+  return (
+    <button
+      className={theme === "dark" ? clickedClass : ""}
+      id="darkMode"
+      onClick={(e) => switchTheme(e)}
+    >Hello World</button>
+  );
+};
+
+export default DarkMode;
