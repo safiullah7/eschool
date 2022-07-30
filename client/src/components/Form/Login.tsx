2203836f63009a39f40cc2d0e8e67a0517663eff
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-07-30 17:22:53 +0500

    Changing links elements and adding toast for signUp component

--- client/src/components/Form/Login.tsx
+++ client/src/components/Form/Login.tsx
@@ -26,8 +26,8 @@ function Login({change}:any) {
           <TitleForm title='Sign In' />
           <TextField label="Email"  fullWidth required size="small" name="email" onChange={handleChange} value={values.email}/>
           <PasswordInput name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
-          <Link to="/reset"><p style={{textAlign: "right", color: "#7A7A7A", fontSize: "14px"}}>Forgot password ?</p></Link>
-          <button type="submit" className='button'>Log In</button>
+          <Link to="/forget"><p style={{textAlign: "right", color: "#7A7A7A", fontSize: "14px"}}>Forgot password ?</p></Link>
+          <Link to="/"><button type="submit" className='button'>Log In</button></Link>
           <HelperLink change={change} text="Don't have an account?" linkText="Sign up" tabNum="2" />
         </form>)}
       </Formik>
--- client/src/components/Form/SignUp.tsx
+++ client/src/components/Form/SignUp.tsx
@@ -4,6 +4,9 @@ import PasswordInput from './PasswordInput';
 import TitleForm from './TitleForm';
 import HelperLink from './HelperLink';
 import { Formik } from 'formik';
+import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface Values {
   email: string;
@@ -14,6 +17,7 @@ interface Values {
 function SignUp({change}:any) {
   const [showPassword, setShowPassword] = useState(false);
   const handleShowPassword = () => setShowPassword(!showPassword);
+  const notify = () => toast("We will send you an email that will allow you to verify your account! Check your account", {position: 'top-center'});
 
   return (
     <>
@@ -28,7 +32,8 @@ function SignUp({change}:any) {
           <PasswordInput name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
           <PasswordInput name="repeatPassword" label="Repeat Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
           <p style={{marginTop: "-5px"}}></p>
-          <button className='button' style={{margin: "40px 0 50px 0"}}>Create Account</button>
+          <Link to="#"><button onClick={notify} className='button' style={{margin: "40px 0 50px 0"}}>Create Account</button></Link>
+          <ToastContainer />
           <HelperLink change={change} text="Have an account?" linkText="Log in" tabNum="1" />
         </form>)}
       </Formik>
