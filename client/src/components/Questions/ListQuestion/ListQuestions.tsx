71aed8f7ebbb86c5197c733772d9118a7ab901c5
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-08-04 11:49:58 +0500

    Fixing some bugs related rendering elements

--- client/src/components/Questions/ListQuestion/ListQuestions.tsx
+++ client/src/components/Questions/ListQuestion/ListQuestions.tsx
@@ -1,29 +1,61 @@
-import React from 'react'
-import { dummyData } from '../dummy'
-import MultipleSelectItem from './ListQuestionTypes/MultipleSelectItem'
-import SingleSelectItem from './ListQuestionTypes/SingleSelectItem'
-import TextInputItem from './ListQuestionTypes/TextInputItem'
-
+import React from "react";
+import { dummyData } from "../dummy";
+import MultipleSelectItem from "./ListQuestionTypes/MultipleSelectItem";
+import SingleSelectItem from "./ListQuestionTypes/SingleSelectItem";
+import TextInputItem from "./ListQuestionTypes/TextInputItem";
 
 function ListOfQuestions() {
-
   return (
-    <div style={{margin: "10px 30px", marginLeft: '40px', padding:"20px 0 40px 0"}}>
-        <div >
-          <div style={{display: 'flex', flexDirection: 'column' }}>
-            {dummyData.map((item) => (
-              item.questionType === 'singleselect' && <><SingleSelectItem questionText={item.questionText} a={item.a} b={item.b} c={item.c} /></>
-              ))} 
-            {dummyData.map((item) => (
-              item.questionType === 'multiselect' && <><MultipleSelectItem questionText={item.questionText} a={item.a} b={item.b} c={item.c} /></>
-              ))} 
-            {dummyData.map((item) => (
-                item.questionType === 'inputtext' && <><TextInputItem questionText={item.questionText} /></>
-            ))}  
-          </div>
-       </div>
+    <div
+      style={{
+        margin: "10px 30px",
+        marginLeft: "40px",
+        padding: "20px 0 40px 0",
+      }}
+    >
+      <div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            maxWidth: "700px",
+            rowGap: "30px",
+          }}
+        >
+          {dummyData.map((item) => {
+            if (item.questionType === "singleselect") {
+              return (
+                <>
+                  <SingleSelectItem
+                    questionText={item.questionText}
+                    a={item.a}
+                    b={item.b}
+                    c={item.c}
+                  />
+                </>
+              );
+            } else if (item.questionType === "multiselect") {
+              return (
+                <>
+                  <MultipleSelectItem
+                    questionText={item.questionText}
+                    a={item.a}
+                    b={item.b}
+                    c={item.c}
+                  />
+                </>
+              );
+            }
+            return (
+              <>
+                <TextInputItem questionText={item.questionText} />
+              </>
+            );
+          })}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ListOfQuestions
+export default ListOfQuestions;
